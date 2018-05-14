504594fe78b5d94b3305b4a5fd6e16681c222841
joeaaron <pant333@163.com>
2018-05-14 09:39:30 +0800

    ...

--- CowaCalib/R1Shell.py
+++ CowaCalib/R1Shell.py
@@ -11,17 +11,22 @@ class Shell:
         self.channel = None
         self.sftp = None
         self.ssh = None
+        # 建立一个sshclient对象
         ssh = paramiko.SSHClient()
+        # 允许将信任的主机自动加入到host_allow 列表，此方法必须放在connect方法的前面
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
+            # 调用connect方法连接服务器
             ssh.connect(ip, port=2223, username = 'root', password = '123456', timeout = 100)
         except:
             return
         self.ssh = ssh
+        # 实例化一个 sftp对象,指定连接的通道
         self.sftp = paramiko.SFTPClient.from_transport(ssh.get_transport())
     def run(self, cmd):
         if self.ssh == None:
             return None
+        # 执行命令
         stdin, stdout, stderr = self.ssh.exec_command(cmd); 
         return stdout.read()
     def startIO(self, cmd):
