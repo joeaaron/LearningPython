4c9030c8484379b9fda6a160fffc6b61bf74ffb8
Unknown <pant333@163.com>
2018-05-23 13:44:23 +0800

    dict class

---
+++ TheHardWay/ex42.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+class TheThing(object):
+	def __init__(self):
+		self.number = 0
+
+	def some_function(self):
+		print "I got called."
+
+	def add_me_up(self, more):
+		self.number += more
+		return self.number
+
+
+# two different things
+a = TheThing()
+b = TheThing()
+
+a.some_function()
+b.some_function()
+
+print a.add_me_up(20)
+print b.add_me_up(30)
+
+print a.number
+print b.number
+
+
+#study this.this is how you pass a variable
+#from one class to another. you will need this.
+
+class TheMultiplier(object):
+	"""docstring for TheMultiplier"""
+	def __init__(self,base):
+		self.base = base	
+
+	def do_it(self, m):
+		return m * self.base
+
+
+x = TheMultiplier(a.number)
+print x.do_it(b.number)
